a9f8cf2e9827d9e68e15256d88f504c2a2259f42
Alex Running <runniale000@gmail.com>
2022-07-18 09:49:08 -0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/navbar";
+import Contact from "./components/contact";
+import Resume from "./components/resume";
+import Portfolio from "./components/portfolio";
+import AboutMe from "./components/aboutMe";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Navbar />
+      <AboutMe />
+      <Portfolio />
+      <Resume />
+      <Contact />
     </div>
   );
 }
